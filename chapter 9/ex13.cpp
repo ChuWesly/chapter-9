a440a197cc83880564b76d35bb20c1bc205340ac
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2020-11-12 00:35:52 +0100

    finalized ex13

--- chapter 9/ex13.cpp
+++ chapter 9/ex13.cpp
@@ -17,11 +17,11 @@ public:
 		if (b == 0) throw Invalid{}; }
 	int num() const { return numerator; };
 	int dem() const { return denominator; };
-	void printt() const;
+	void print() const;
 	double rat2double() const;
 };
 
-void rational::printt() const{
+void rational::print() const{
 	cout << numerator << "/" << denominator << '\n';
 }
 
@@ -33,7 +33,6 @@ double rational::rat2double() const{
 bool operator==(const rational& a, const rational& b) {
 	int aa = a.num() * b.dem();
 	int bb = b.num() * a.dem();
-	//cout << aa << bb << "its work" << (aa == bb)  << '\n';
 	return (aa == bb);
 }
 
@@ -41,13 +40,42 @@ bool operator !=(const rational& a, const rational& b) {
 	return !(a == b);
 }
 
+rational operator+(const rational& a, const rational& b) {
+	int anum = a.num() * b.dem();
+	int bnum = b.num() * a.dem();
+	int num = anum + bnum;
+	int dem = a.dem() * b.dem();
+	return rational(num, dem);
+}
+
+rational operator-(const rational& a, const rational& b) {
+	int anum = a.num() * b.dem();
+	int bnum = b.num() * a.dem();
+	int num = anum - bnum;
+	int dem = a.dem() * b.dem();
+	return rational(num, dem);
+}
+
+rational operator*(const rational& a, const rational& b) {
+	int num = a.num() * b.num();
+	int dem = a.dem() * b.dem();
+	return rational(num, dem);
+}
+
+rational operator/(const rational& a, const rational& b) {
+	int num = a.num() * b.dem();
+	int dem = a.dem() * b.num();
+	return rational(num, dem);
+}
+
 int main() {
 	cout << "hello" << '\n';
 	rational half{ 1,3 };
 	rational half4{ 3,6 };
+	rational add = half / half4;
 	bool test{ half != half4 };
 	if (test) {
 		cout << half.rat2double() << '\n';
 	}
-	cout << test;
+	add.print();
 }
--- chapter 9/ex2.cpp
+++ chapter 9/ex2.cpp
@@ -1,43 +1,155 @@
 /*Exercise 2 of chapter 9*/
+//  Design and implement a Name_pairs class holding (name,age) pairs where name
+//  is a string and age is a double. Represent that as a vector<string>
+//  (called name) and a vector<double> (called age) member. Provide an input
+//  operation read_names() that reads a series of names. Provide a read_ages()
+//  operation that prompts the user for an age for each name. Provide a
+//  print() operation that prints out the (name[i],age[i]) pairs (one per line)
+//  in the order determined by the name vector. Provide a sort() operation
+//  that sorts the name vector in alphabetical order and reorganize the age
+//  vector to match. Implement all "operations" as member functions. Test the
+//  class (of course: test early an often).
 
 #include "../../std_lib_facilities.h"
 
 class Name_pairs {
-	vector<string> name;
-	vector<double> age;
 public:
-	Name_pairs() :
-		name{"test"} {};
-	void print();
-	void read_names(istream& input);
-	void read_ages(istream& input);
+    void read_names();      // read a series of names
+    void read_ages();       // read the ages for each names
+
+    void print() const;     // print pairs of (name,age) sorted by name
+    void sort();            // sort names applying that order to their
+                            // related ages
+private:
+    const string stop{ "stop" };  // command to stop read_names()
+    vector<string> name;
+    vector<double> age;
+
+    bool is_in(const string& n) const; // check if string s is already in name
+    size_t name_idx(const string& n) const; // return name index for n
 };
 
-void Name_pairs::read_names(istream& input) {
-	string n;
-	while (input >> n) {
-		
-		name.push_back(n);
-	}
+bool Name_pairs::is_in(const string& n) const
+// Checks if string n is already present on name vector.
+{
+    for (string x : name)
+        if (x == n) return true;
+    return false;
 }
 
-void Name_pairs::read_ages(istream& input) {
-	double n;
-	while (input >> n) {
-		age.push_back(n);
-	}
+void Name_pairs::read_names()
+// Asks the user for names and adds them to name vector until the user writes
+// stop.
+// No duplicate names are permitted.
+{
+    cout << "Write people names (write stop when there's enough)...\n: ";
+    string n;
+    while (cin >> n && n != stop) {
+        if (is_in(n))
+            cout << "Duplicated name " << n << "! Try again ...\n";
+        else
+            name.push_back(n);
+        cout << ": ";
+    }
 }
 
-void Name_pairs::print() {
-	for (string i : name) {
-		cout << i << '\n';
-	}
+void Name_pairs::read_ages()
+// Get ages for people in name vector.
+{
+    cout << "Give me the ages of each person, please ...\n";
+    for (string n : name) {
+        cout << "How old is " << n << "? ";
+        double a{ 0 };
+        while (!(cin >> a)) {
+            // Make sure that we read a number.
+            cout << "Sorry, I don't understand you! How old is " << n << "? ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        age.push_back(a);
+    }
 }
 
-int main() {
-	Name_pairs test{};
-	istream& input = cin;
-	test.read_names(input);
-	test.print();
+size_t Name_pairs::name_idx(const string& n) const
+// Returns the index at which a name (n) is located on a vector (name).
+// As the program goes we are almost sure the name will be present, so an error
+// is thrown otherwise.
+{
+    size_t limit = name.size();
+    for (size_t i = 0; i < limit; ++i)
+        if (name[i] == n) return i;
+    error("Name_pairs::name_idx(): No element found!");
+}
 
+void Name_pairs::sort()
+// Sort name vector and extends the position changes to the age vector The
+// vectors must be the same size.
+{
+    if (name.size() != age.size())
+        error("Name_pairs::sort(): name and age"
+            "sizes differ (no age read performed?)");
+
+    vector<string> s_name{ name };   // name work copy
+    vector<double> s_age{ age };     // age work copy
+
+    // Perform sort on copies, not modifying the object state until the end
+    using std::sort;      // Compiler only saw a candidate; Name_pairs::sort()
+                          // so it must be forced. We could also use
+                          // std::sort() instead.
+    sort(s_name.begin(), s_name.end());
+    size_t limit = s_name.size();
+    for (size_t i = 0; i < limit; ++i)
+        s_age[i] = age[name_idx(s_name[i])];
+
+    // If everything goes right, exchange the vectors
+    name = s_name;
+    age = s_age;
+}
+
+void Name_pairs::print() const
+// Prints a line with each name and its respective age, from the same both
+// elements of two distinct vectors. Printing are done sorted by name.
+{
+    if (name.size() != age.size())
+        error("Name_pairs::print(): name and age"
+            "sizes differ (no age read performed?)");
+
+    vector<string> s_name{ name };   // name work copy
+    vector<double> s_age{ age };     // age work copy
+
+    // First sort the copies
+    std::sort(s_name.begin(), s_name.end());
+    size_t limit = s_name.size();
+    for (size_t i = 0; i < limit; ++i)
+        s_age[i] = age[name_idx(s_name[i])];
+
+    // Now print sorted
+    for (size_t i = 0; i < limit; ++i)
+        cout << "\t(" << s_name[i] << ", " << s_age[i] << ")\n";
+}
+
+int main()
+try {
+    Name_pairs pairs;
+
+    pairs.read_names();
+    pairs.read_ages();
+    cout << "\nOK! So you tell me about these people:\n";
+    pairs.print();
+    pairs.sort();
+    cout << "\nThat sorted alphabetically by name are:\n";
+    pairs.print();
+    cout << '\n';
+
+    return 0;
+}
+catch (exception& e)
+{
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch (...)
+{
+    cerr << "Unknown exception!\n";
+    return 2;
 }
